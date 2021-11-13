396167248ca509de7c0b66489f5fe8ba2b50f664
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-11-13 21:38:42 +0600

    fs module

--- index.js
+++ index.js
@@ -4,7 +4,33 @@
 // console.log(people.test);
 // console.log(people.people);
 
-const path=require('path');
-const pathname= 'E:\Node Sum\index.js';
+////// Path modules///
 
-console.log(path.basename(pathname));
+
+// const path=require('path');
+// const pathname= 'E:\Node Sum\index.js';
+
+// console.log(path.basename(pathname));
+
+
+
+
+//  OS Module
+
+// const os=require('os');
+// // console.log(os.platform());
+// // console.log(os.totalmem());
+
+// console.log(os.cpus());
+
+
+
+
+// FS Module
+const fs=require('fs');
+
+fs.readFile('mynewText.txt',(err,data)=>{
+    console.log(data.toString());
+});
+
+console.log('hello');
