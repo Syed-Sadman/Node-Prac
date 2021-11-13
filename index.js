4172be007df09325fc6ca5505743c76663f9dab9
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-11-13 16:39:02 +0600

    path modules

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const people=require('./people.js');
+// const people=require('./people.js');
 
-console.log(people.a);
-console.log(people.test);
-console.log(people.people);
+// console.log(people.a);
+// console.log(people.test);
+// console.log(people.people);
+
+const path=require('path');
+const pathname= 'E:\Node Sum\index.js';
+
+console.log(path.basename(pathname));
