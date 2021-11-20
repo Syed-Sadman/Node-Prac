a5a0dd387e018cfefa24214e14b41ee88180e0e0
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-11-20 12:49:42 +0600

    incoming request

--- http.js
+++ http.js
@@ -1,10 +1,13 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
     if(req.url=='/'){    
-        res.write('Hello programmers');
+        res.write('<html><head><title>Form</title></head></html>');
+        res.write('<body><form method="post" action="/process"><input name="message"/></form></body>')
         res.end();
-    }else if(req.url=='/about-us'){
-        
+    }else if(req.url=='/process' && req.method=='POST'){
+        res.write('data', (chunk)=>{
+            console.log(chunk);
+        })
         res.write('welcome to about us page');
         res.end();
     }else{
