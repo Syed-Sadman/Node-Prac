eeff2c63ae08318f3cc30d64850455c0a3bd8ba5
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-11-21 11:22:38 +0600

    readable stream res pipe

--- http.js
+++ http.js
@@ -1,20 +1,28 @@
 const http=require('http');
-const server=http.createServer((req,res)=>{
-    if(req.url=='/'){    
-        res.write('<html><head><title>Form</title></head></html>');
-        res.write('<body><form method="post" action="/process"><input name="message"/></form></body>')
-        res.end();
-    }else if(req.url=='/process' && req.method=='POST'){
-        res.write('data', (chunk)=>{
-            console.log(chunk);
-        })
-        res.write('welcome to about us page');
-        res.end();
-    }else{
-        res.write('page not found 404 error');
-        res.end();
+const fs=require('fs');
 
-    }
+const server=http.createServer((req,res)=>{
+
+    const readStreamData=fs.createReadStream(`${__dirname}/writeData.txt`);
+
+    readStreamData.pipe(res);
+    
+  
+    // if(req.url=='/'){    
+    //     res.write('<html><head><title>Form</title></head></html>');
+    //     res.write('<body><form method="post" action="/process"><input name="message"/></form></body>')
+    //     res.end();
+    // }else if(req.url=='/process' && req.method=='POST'){
+    //     res.write('data', (chunk)=>{
+    //         console.log(chunk);
+    //     })
+    //     res.write('welcome to about us page');
+    //     res.end();
+    // }else{
+    //     res.write('page not found 404 error');
+    //     res.end();
+
+    // }
     
 });
 
