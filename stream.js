49bcd6a156c499f6794734ffff9dc73018d8c75c
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-11-21 11:15:22 +0600

    pipe method

--- stream.js
+++ stream.js
@@ -1,9 +1,11 @@
 const fs= require('fs');
 
 const readData=fs.createReadStream(`${__dirname}/mynewText.txt`);
+const writeData=fs.createWriteStream(`${__dirname}/writeData.txt`);
 
-readData.on('data',(data)=>{
-    console.log(data);
-})
 
-console.log('hello');
+// readData.on('data',(chunk)=>{
+//     writeData.write(chunk);
+// })
+//this pipe method can do the same task in a most friendly way 
+readData.pipe(writeData);
