7c6d718a24b0228239b7078214581343be703fbb
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-11-25 12:52:22 +0600

    mountpath

--- rawexp.js
+++ rawexp.js
@@ -2,19 +2,29 @@ const express=require('express');
 
 const app=express();
 
-app.use(express.static(`${__dirname}/public`,{
-    index:'home.html'
-    })
-);
+const admin=express();
+
+    //static express
+// app.use(express.static(`${__dirname}/public`,{
+//     index:'home.html'
+//     })
+// );
 
 
-app.get('/',(req,res)=>{ 
-    res.send('This is the homepage');
+admin.get('/dashboard',(req,res)=>{ 
+    console.log(admin.mountpath);
+    res.send('welcome to admin dashboard');
 })
 
-app.post('/',(req,res)=>{
-    res.send('This is the homepage with post request');
+app.get('/',(req,res)=>{
+    res.send('welcome to homepage');
 })
+
+app.use('/admin',admin);
+
+// app.post('/',(req,res)=>{
+//     res.send('This is the homepage with post request');
+// })
 
 
 app.listen(3000,()=>{
