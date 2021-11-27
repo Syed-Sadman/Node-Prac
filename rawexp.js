07bd251d8bfeecb0eb7170181488dad6979421af
Syed Sadman Sabbir <sadman.syed@yahoo.com>
2021-11-27 12:17:36 +0600

    express param()

--- rawexp.js
+++ rawexp.js
@@ -11,16 +11,28 @@ const admin=express();
 // );
 
 
-admin.get('/dashboard',(req,res)=>{ 
-    console.log(admin.mountpath);
-    res.send('welcome to admin dashboard');
+// admin.get('/dashboard',(req,res)=>{ 
+//     console.log(admin.mountpath);
+//     res.send('welcome to admin dashboard');
+// })
+
+
+app.param('id',(req,res,next,id)=>{
+    const user={
+        name:id,
+        identity:'Bangladesh'
+    }
+    req.userDetails=user;
+    
+    next();
 })
 
-app.get('/',(req,res)=>{
+app.get('/user/:id',(req,res)=>{
+    console.log(req.userDetails);
     res.send('welcome to homepage');
 })
 
-app.use('/admin',admin);
+// app.use('/admin',admin);
 
 // app.post('/',(req,res)=>{
 //     res.send('This is the homepage with post request');
